424c75845c1342958fb84da02790bc065ddd1542
MASTER217 <103946950+MASTER217@users.noreply.github.com>
2022-04-18 20:55:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
+        self.API_ID: str = os.environ.get("API_ID", 19217751)
+        self.API_HASH: str = os.environ.get("API_HASH", 5367033476:AAFYsFMxGRZXNQScfqNNnOED25-Rh-HUPDM)
+        self.SESSION: str = os.environ.get("SESSION", BQBi1hARdGbSNhAvd3pjHQsyASgPtiHlxyQkh9noX2dGwTrugzFCbHhDT8ADnSpG73dUuOZ5SOtK8TAiDinpPq5FU9N9g5aLgWL2AvpRTX2Q4GEJhuh9BhkJB2uL4_Sz_MuVXe-n9DJNiN1DucO_nhYCaSj0Vd-YhhFq8nxqRjvc-Gf0F_XIwNPMo6ICKg4AHJF3Rpmx4ZrSn9fxl5kMW-9PIfK9smWbBy3G-CFUSA4AVwmrkBGVVi-2xT7u4EEr2aPzZR9ebqr-H82VPIdi5XRSKYiJ8yCjmCCfuXzTWzKoLh_3f5sM0qHTB6a94CKML_Sv_GJJL0CscQJEFCWPIzwvAAAAATg_3UsA)
+        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", 5396090877:AAEmPoFlKb98wJFzsAXv8GPPPNwQ_JG5P4g)
         self.SUDOERS: list = [
             int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
         ]
